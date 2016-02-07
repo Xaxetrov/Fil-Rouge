6bb42177fc093b11dd43abf020bad1a1f2ee683b
Xaxetrov <edern.haumont@orange.fr>
2016-02-07 23:47:55 +0100

    Sebastien worked on XML

--- SaveManager.cpp
+++ SaveManager.cpp
@@ -10,7 +10,7 @@ SaveManager::SaveManager()
 
 int SaveManager::SaveNetwork(const NeuralNetwork& nn, QString neuralNetworkName)
 {
-    QString fileName = savePath+neuralNetworkName+".xml";
+    /*QString fileName = savePath+neuralNetworkName+".xml";
     QDomDocument dom(neuralNetworkName);
     QFile doc_xml(fileName);
     std::cerr << fileName.toStdString();
@@ -26,7 +26,53 @@ int SaveManager::SaveNetwork(const NeuralNetwork& nn, QString neuralNetworkName)
         doc_xml.close();
         return 2;
     }
-    doc_xml.close();
+    doc_xml.close();*/
+
+
+    QString fileXmlName = savePath+neuralNetworkName+".xml";
+    QFile fileXml(fileXmlName);
+
+    // Ouverture du fichier en Ã©criture et en texte. (sort de la fonction si le fichier ne s'ouvre pas)
+    if(!fileXml.open(QFile::WriteOnly | QFile::Text))
+    {   return false;
+    }
+    QXmlStreamWriter writer(&fileXml);
+
+    // Active l'indentation automatique du fichier XML pour une meilleur visibilitÃ©
+    writer.setAutoFormatting(true);
+
+    // Insert la norme de codification du fichier XML :
+    writer.writeStartDocument();
+
+    // Ã‰lÃ©ment racine du fichier XML
+    writer.writeStartElement("NeuronalNetwork");
+
+    int m_hiddenLayerNum=5;   // arbitraire
+
+    writer.writeStartElement("NeuronLayer");
+    writer.writeStartElement("NeuronLayerNumber");
+    writer.writeCharacters("inputLayer");
+    writer.writeEndElement();
+    writer.writeEndElement();
+
+    for (int i=0;i<m_hiddenLayerNum;i++)
+    {   writer.writeStartElement("NeuronLayer");
+        writer.writeStartElement("NeuronLayerNumber");
+        writer.writeCharacters(QString::number(1));
+        writer.writeEndElement();
+        writer.writeEndElement();
+    }
+
+    writer.writeStartElement("NeuronLayer");
+    writer.writeStartElement("NeuronLayerNumber");
+    writer.writeCharacters("outputLayer");
+    writer.writeEndElement();
+    writer.writeEndElement();
+
+    writer.writeEndDocument();
+
+    fileXml.close();
+
 
     return 0;
 }
