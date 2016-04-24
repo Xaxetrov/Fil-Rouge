3e9e377f1c41280219ee8dc0077c2f9dd65b3281
Xaxetrov <edern.haumont@orange.fr>
2016-04-25 00:42:21 +0200

    added an "age" variable to use to pick the best animals

--- Animal.cpp
+++ Animal.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 Animal::Animal(double x, double y, int radius, int maxSpeed, double damage, World * world) :
-    Solid(x, y, radius), m_maxSpeed(maxSpeed), m_damage(damage), m_world(world)
+    Solid(x, y, radius), m_maxSpeed(maxSpeed), m_damage(damage), m_world(world), m_age(0)
 {
     m_angle = 0; //initialize angle here
     m_hunger = 0;
@@ -59,7 +59,7 @@ Animal::~Animal()
 
 int Animal::play()
 {
-
+    m_age++;
     if(m_health <= 0 || dead)
     {
       dead = true;
@@ -396,6 +396,11 @@ vector<weak_ptr<Entity>> Animal::getSubListResourceCollision()
         }
     }
     return subListCollision;
+}
+
+int Animal::getAge() const
+{
+    return m_age;
 }
 
 int Animal::getMaxSpeed() const
